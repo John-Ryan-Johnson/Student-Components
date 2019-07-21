a78ac37f37612ea8b4756672751a485c6aa86d99
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-07-20 19:52:41 -0500

    student-components

---
+++ main.js
@@ -0,0 +1,97 @@
+// ------ Practice: Student Components ------
+const createStudentComponent = (name, subject, info, cls) => {
+  return `
+      <div class="student">
+          <h1 class="xx-large ${cls}">${name}</h1>
+          <section class="bordered dashed section--padded">${subject}</section>
+          <aside class="pushRight">${info}</aside>
+      </div>
+  `
+}
+
+const studentContainer = document.querySelector("#container")
+
+for (const student of students) {
+  let studentComponent = ""
+  if (student.score >= 60) {
+    studentComponent = createStudentComponent(
+      student.name,
+      student.subject,
+      student.info,
+      "passing"
+    )
+
+  } else {
+    studentComponent = createStudentComponent(
+      student.name,
+      student.subject,
+      student.info,
+      "failing"
+    )
+  }
+  // studentContainer.innerHTML += studentComponent
+}
+
+
+// ------ Practice: One Object to Rule Them All ------
+
+const createStudentComponent1 = (object, cls) => {
+  return `
+      <div class="student">
+          <h1 class="xx-large ${cls}">${object.name}</h1>
+          <section class="bordered dashed section--padded">${object.subject}</section>
+          <aside class="pushRight">${object.info}</aside>
+      </div>
+  `
+}
+
+const studentContainer1 = document.querySelector("#container")
+
+for (const student of students) {
+  let studentComponent = ""
+  if (student.score >= 60) {
+    studentComponent = createStudentComponent1(student, "passing")
+
+  } else {
+    studentComponent = createStudentComponent1(student, "failing")
+  }
+  // studentContainer1.innerHTML += studentComponent
+}
+
+// ------ Challenge: Composition of Smaller Components ------
+// Write functions that build the sub-components of the larger student component.
+
+const createH1 = (studentObject, cls) => {
+  return `<h1 class="xx-large ${cls}">${studentObject}</h1>`
+}
+
+const createSection = (studentObject) => {
+  return `<section class="bordered dashed section--padded">${studentObject}</section>`
+}
+
+const createAside = (studentObject) => {
+  return `<aside class="pushRight">${studentObject}</aside>`
+}
+
+const createStudentComponent2 = (studentObject, cls) => {
+  return `
+    <div class="student">
+      ${createH1(studentObject.name, cls)}
+      ${createSection(studentObject.subject)}
+      ${createAside(studentObject.info)}
+    </div>
+  `
+}
+
+const studentContainer2 = document.querySelector("#container")
+
+for (const student of students) {
+  let studentComponent = ""
+  if (student.score >= 60) {
+    studentComponent = createStudentComponent2(student, "passing")
+
+  } else {
+    studentComponent = createStudentComponent2(student, "failing")
+  }
+  studentContainer2.innerHTML += studentComponent
+}
